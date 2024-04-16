f420755726dece09a0b9a8b36ec694abe6476598
Alex Ivan Jiménez Cen <131420833+AlexCen29@users.noreply.github.com>
2024-04-15 18:06:05 -0600

    Update main_page.dart
    cambios

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -20,6 +20,7 @@ class _MainPageState extends State<MainPage> {
   List<BluetoothDevice> _devices = [];
   BluetoothDevice? _deviceConnected;
   int times = 0;
+  int counter = 0;
 
   void _getDevices() async {
     var res = await _bluetooth.getBondedDevices();
@@ -28,8 +29,12 @@ class _MainPageState extends State<MainPage> {
 
   void _receiveData() {
     _connection?.input?.listen((event) {
-      if (String.fromCharCodes(event) == "p") {
-        setState(() => times = times + 1);
+      String data = String.fromCharCodes(event).trim();
+      int? newCounter = int.tryParse(data);
+      if (newCounter != null) {
+        setState(() {
+          counter = newCounter;
+        });
       }
     });
   }
@@ -74,7 +79,7 @@ class _MainPageState extends State<MainPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('Flutter ❤️ Arduino'),
+        title: const Text('Flutter ❤ Arduino'),
       ),
       body: Column(
         children: [
@@ -139,15 +144,12 @@ class _MainPageState extends State<MainPage> {
                           child: const Text('conectar'),
                           onPressed: () async {
                             setState(() => _isConnecting = true);
-
                             _connection = await BluetoothConnection.toAddress(
                                 device.address);
                             _deviceConnected = device;
                             _devices = [];
                             _isConnecting = false;
-
                             _receiveData();
-
                             setState(() {});
                           },
                         ),
@@ -158,7 +160,6 @@ class _MainPageState extends State<MainPage> {
             ),
           );
   }
-
   Widget _buttons() {
     return FadeInUp(
       animate: _connection?.isConnected ?? false,
@@ -167,32 +168,80 @@ class _MainPageState extends State<MainPage> {
         color: Colors.black12,
         child: Column(
           children: [
-            const Text('Controles para LED', style: TextStyle(fontSize: 18.0)),
+            const Text('Controles para cinta transportadora',
+                style: TextStyle(fontSize: 18.0)),
             const SizedBox(height: 18.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 MyButtonLed(
-                    titulo: "verde",
+                    titulo: "Motor",
                     data: "1",
-                    data2: "2",
+                    data2: "0",
                     color: Colors.green,
                     connection: _connection),
-                MyButtonLed(
-                    titulo: "Amarillo",
-                    data: "3",
-                    data2: "4",
-                    color: Colors.yellow,
-                    connection: _connection),
-                MyButtonLed(
-                  titulo: "Rojo",
-                  data: "5",
-                  data2: "6",
-                  color: Colors.red,
-                  connection: _connection,
-                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _sendData("2");
+                  },
+                  child: Text(
+                    "Reset",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+                    minimumSize: MaterialStateProperty.all<Size>(Size(MediaQuery.of(context).size.width * 0.3, MediaQuery.of(context).size.height * 0.08)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
+            const SizedBox(height: 18.0),
+            ElevatedButton(
+              onPressed: () {
+                _sendData("3");
+              },
+              child: const Text(
+                "Reproducir Canción",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                ),
+                padding: MaterialStateProperty.all<EdgeInsets>(
+                  EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                padding: const EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Text(
+                  'Contador: $counter',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
@@ -253,201 +302,10 @@ class _MyButtonLedState extends State<MyButtonLed> {
           width: screenSize.width * 0.3,
           height: screenSize.height * 0.08,
           child: Center(
-            child: Text(widget.titulo),
+            child: Text(widget.titulo,style: const TextStyle(color: Colors.white),),
           ),
         ),
       ),
     );
   }
 }
-
-// import 'dart:convert';
-// import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
-// import 'package:flutter/material.dart';
-// import 'package:permission_handler/permission_handler.dart';
-// import 'package:animate_do/animate_do.dart';
-
-// class MainPage extends StatefulWidget {
-//   const MainPage({super.key});
-
-//   @override
-//   State<MainPage> createState() => _MainPageState();
-// }
-
-// class _MainPageState extends State<MainPage> {
-//   final _bluetooth = FlutterBluetoothSerial.instance;
-//   bool _bluetoothState = false;
-//   bool _isConnecting = false;
-//   BluetoothConnection? _connection;
-//   List<BluetoothDevice> _devices = [];
-//   BluetoothDevice? _deviceConnected;
-//   int times = 0;
-//   final _textController = TextEditingController();
-
-//   void _getDevices() async {
-//     var res = await _bluetooth.getBondedDevices();
-//     setState(() => _devices = res);
-//   }
-
-//   void _receiveData() {
-//     _connection?.input?.listen((event) {
-//       if (String.fromCharCodes(event) == "p") {
-//         setState(() => times = times + 1);
-//       }
-//     });
-//   }
-
-//   void _sendData(String data) {
-//     if (_connection?.isConnected ?? false) {
-//       _connection?.output.add(ascii.encode(data));
-//     }
-//   }
-
-//   void _requestPermission() async {
-//     await Permission.location.request();
-//     await Permission.bluetooth.request();
-//     await Permission.bluetoothScan.request();
-//     await Permission.bluetoothConnect.request();
-//   }
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     _requestPermission();
-
-//     _bluetooth.state.then((state) {
-//       setState(() => _bluetoothState = state.isEnabled);
-//     });
-
-//     _bluetooth.onStateChanged().listen((state) {
-//       switch (state) {
-//         case BluetoothState.STATE_OFF:
-//           setState(() => _bluetoothState = false);
-//           break;
-//         case BluetoothState.STATE_ON:
-//           setState(() => _bluetoothState = true);
-//           break;
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         centerTitle: true,
-//         title: const Text('Flutter ❤️ Arduino'),
-//       ),
-//       body: Column(
-//         children: [
-//           _controlBT(),
-//           _infoDevice(),
-//           Expanded(child: _listDevices()),
-//           _buttons(),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget _controlBT() {
-//     return SwitchListTile(
-//       value: _bluetoothState,
-//       onChanged: (bool value) async {
-//         if (value) {
-//           await _bluetooth.requestEnable();
-//         } else {
-//           await _bluetooth.requestDisable();
-//         }
-//       },
-//       tileColor: Colors.black26,
-//       title: Text(
-//         _bluetoothState ? "Bluetooth encendido" : "Bluetooth apagado",
-//       ),
-//     );
-//   }
-
-//   Widget _infoDevice() {
-//     return ListTile(
-//       tileColor: Colors.black12,
-//       title: Text("Conectado a: ${_deviceConnected?.name ?? "ninguno"}"),
-//       trailing: _connection?.isConnected ?? false
-//           ? TextButton(
-//               onPressed: () async {
-//                 await _connection?.finish();
-//                 setState(() => _deviceConnected = null);
-//               },
-//               child: const Text("Desconectar"),
-//             )
-//           : TextButton(
-//               onPressed: _getDevices,
-//               child: const Text("Ver dispositivos"),
-//             ),
-//     );
-//   }
-
-//   Widget _listDevices() {
-//     return _isConnecting
-//         ? const Center(child: CircularProgressIndicator())
-//         : SingleChildScrollView(
-//             child: Container(
-//               color: Colors.grey.shade100,
-//               child: Column(
-//                 children: [
-//                   ...[
-//                     for (final device in _devices)
-//                       ListTile(
-//                         title: Text(device.name ?? device.address),
-//                         trailing: TextButton(
-//                           child: const Text('conectar'),
-//                           onPressed: () async {
-//                             setState(() => _isConnecting = true);
-
-//                             _connection = await BluetoothConnection.toAddress(
-//                                 device.address);
-//                             _deviceConnected = device;
-//                             _devices = [];
-//                             _isConnecting = false;
-
-//                             _receiveData();
-
-//                             setState(() {});
-//                           },
-//                         ),
-//                       )
-//                   ]
-//                 ],
-//               ),
-//             ),
-//           );
-//   }
-
-//   Widget _buttons() {
-//     return FadeInUp(
-//       animate: _connection?.isConnected ?? false,
-//       child: Container(
-//         padding: const EdgeInsets.symmetric(vertical: 24.0, horizontal: 8.0),
-//         color: Colors.black12,
-//         child: Column(
-//           children: [
-//             const Text('Escribe un mensaje para el LCD', style: TextStyle(fontSize: 18.0)),
-//             const SizedBox(height: 18.0),
-//             TextField(
-//               controller: _textController,
-//               decoration: InputDecoration(
-//                 border: OutlineInputBorder(),
-//                 labelText: 'Mensaje',
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 _sendData(_textController.text);
-//               },
-//               child: Text('Enviar'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
